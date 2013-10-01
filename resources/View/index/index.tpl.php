97ca1a5aca5c7de6aebd66643f84709b0a25c466
Davide Gullo <jazzo72@gmail.com>
2013-10-01 14:11:56 +0200

    New HomePage frontend

--- resources/View/index/index.tpl.php
+++ resources/View/index/index.tpl.php
@@ -1,4 +1,4 @@
 <div class="jumbotron">
     <p class="lead">Open Source Software per i Gruppi di acquisto!</p>
-    <p><a class="btn btn-lg btn-success" href="#">Versione 1.1</a></p>
+    <p><a class="btn btn-lg btn-primary" href="#">Versione 1.1</a></p>
 </div>
