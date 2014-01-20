30919cfbe30e388d0eb53c223e541b6338c8f451
Davide Gullo <jazzo72@gmail.com>
2014-01-21 00:46:24 +0100

    ADDED Logger per variazioni su Ordini

---
+++ resources/Model/Ordini/Logger.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Description of Logger
+ * 
+ * @author gullo
+ */
+class Model_Ordini_Logger {
+    
+    static function LogToDB($idordine, $descrizione) 
+    {
+        $db = Zend_Registry::get("db");
+        $sth = $db->prepare("INSERT INTO ordini_variazioni SET idordine= :idordine, data=NOW(), descrizione= :descrizione");
+        return $sth->execute(array('idordine' => $idordine, 'descrizione' => $descrizione));
+    }
+    
+    
+    static function LogVariazionePrezzo($idordine, $idprodotto, $pre, $new)
+    {
+        $pObj = new Model_Prodotti();
+        $prod = $pObj->getProdottoById($idprodotto);
+        if(!is_null($prod))
+        {
+            $descProdotto = $prod->descrizione;
+            $txt = "Il prezzo del prodotto <b>$descProdotto</b> è stato modificato, da $pre &euro; a <b>$new &euro;</b>";
+            self::LogToDB($idordine, $txt);
+        }
+    }
+    
+}
