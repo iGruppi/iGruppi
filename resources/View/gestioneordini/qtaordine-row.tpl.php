980008fab44cd7c03b1e02b156fdfbb465bc3f22
Davide Gullo <jazzo72@gmail.com>
2014-02-26 11:14:30 +0100

    REMOVED getQtaReale (use always qta_reale when ask for getQta)

--- resources/View/gestioneordini/qtaordine-row.tpl.php
+++ resources/View/gestioneordini/qtaordine-row.tpl.php
@@ -1,14 +1,14 @@
 <td><?php echo $pObj->descrizione;?><br /><?php echo $pObj->getPrezzo();?> &euro; / <?php echo $pObj->udm; ?> (Codice: <?php echo $pObj->codice;?>)</td>
-<td><strong><?php echo $pObj->getQta();?></strong></td>
+<td><strong><?php echo $pObj->getQtaOrdinata();?></strong></td>
 <td>
 <?php if($pObj->isDisponibile()): ?>
     <form id="qta_ord_form_<?php echo $iduser; ?>_<?php echo $idprodotto;?>" class="ordini" onsubmit="jx_RefModQta_Save(<?php echo $iduser; ?>,<?php echo $idprodotto;?>,<?php echo $this->ordine->idordine;?>); return false;" method="post">
         <input type="number" class="field_in_table" id="qta_eff_<?php echo $iduser; ?>_<?php echo $idprodotto;?>"
                pattern="[0-9]+([\.|,][0-9]+)?" step="0.001" required data-loading-text="Loading..."
-               name="qta_reale" value="<?php echo $pObj->getQtaReale();?>" 
+               name="qta_reale" value="<?php echo $pObj->getQta();?>" 
                title="Deve essere un numero, per i decimali utilizzare o il punto (.) o la virgola (,)" 
                onchange="jx_ReferenteModifyQta(<?php echo $iduser; ?>,<?php echo $idprodotto;?>)" />
-        <input type='hidden' id="qta_eff_old_<?php echo $iduser; ?>_<?php echo $idprodotto;?>" value="<?php echo $pObj->getQtaReale();?>" />
+        <input type='hidden' id="qta_eff_old_<?php echo $iduser; ?>_<?php echo $idprodotto;?>" value="<?php echo $pObj->getQta();?>" />
         <input type='hidden' name="idordine" value="<?php echo $this->ordine->idordine;?>" /> 
         <input type='hidden' name="iduser" value="<?php echo $iduser;?>" />
         <input type='hidden' name="idprodotto" value="<?php echo $idprodotto;?>" />
