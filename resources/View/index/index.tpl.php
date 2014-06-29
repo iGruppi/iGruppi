0230473cf538e0d16870ffd44ecf221014e791dc
Davide Gullo <jazzo72@gmail.com>
2014-06-29 17:34:41 +0200

    Set versione 1.4

--- resources/View/index/index.tpl.php
+++ resources/View/index/index.tpl.php
@@ -1,4 +1,4 @@
 <div class="jumbotron">
     <p class="lead">Open Source Software per i Gruppi di acquisto!</p>
-    <p><a class="btn btn-lg btn-success" href="#">Versione 1.3</a></p>
+    <p><a class="btn btn-lg btn-success" href="#">Versione 1.4</a></p>
 </div>
