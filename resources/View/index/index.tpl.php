47558a0f5ce1a93443f8eb771b3853430ffb422d
Davide Gullo <jazzo72@gmail.com>
2015-03-13 10:42:43 +0100

    Start 2.0 Beta test!

--- resources/View/index/index.tpl.php
+++ resources/View/index/index.tpl.php
@@ -1,4 +1,4 @@
 <div class="jumbotron">
     <p class="lead">Software per reti di Gruppi di acquisto</p>
-    <p><a class="btn btn-lg btn-success" href="http://www.igruppi.com/download/">Versione 1.4</a></p>
+    <p><a class="btn btn-lg btn-success" href="http://www.igruppi.com/download/">Versione 2.0_beta</a></p>
 </div>
