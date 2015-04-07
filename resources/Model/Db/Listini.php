416d09d3b828754d3dbd3ef00441cd0ed5a9a115
Davide Gullo <jazzo72@gmail.com>
2015-04-07 13:50:03 +0200

    ... Only prodotti Attivi e in Production

--- resources/Model/Db/Listini.php
+++ resources/Model/Db/Listini.php
@@ -99,7 +99,7 @@ class Model_Db_Listini extends MyFw_DB_Base {
     function addProdottiToListinoByIdProduttore($idlistino, $idproduttore)
     {
         $sql = "INSERT INTO listini_prodotti (idlistino, idprodotto, descrizione_listino, costo_listino, note_listino)
-                SELECT :idlistino, idprodotto, descrizione, costo, note FROM prodotti WHERE idproduttore= :idproduttore";
+                SELECT :idlistino, idprodotto, descrizione, costo, note FROM prodotti WHERE idproduttore= :idproduttore AND attivo='S' AND production='S'";
         $sth = $this->db->prepare($sql);
         $sth->execute(array('idlistino' => $idlistino, 'idproduttore' => $idproduttore));
     }
