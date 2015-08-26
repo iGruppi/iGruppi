ce3f90a4186b8fca177539bfc552cfe9ee545e86
Davide Gullo <jazzo72@gmail.com>
2015-08-26 16:12:40 +0200

    Modifiche DB: moved ordini.iduser_ref -> iduser_supervisore and ordini_groups.iduser_ref -> iduser_incaricato

--- documentation/database/igruppi_dump_2.sql
+++ documentation/database/igruppi_dump_2.sql
@@ -1,29 +1,11 @@
--- phpMyAdmin SQL Dump
--- version 3.3.3
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Aug 19, 2015 at 10:38 pm
--- Server version: 5.1.54
--- PHP Version: 5.3.14
-
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-
+SET time_zone = "+00:00";
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
 
---
--- Database: `igruppi`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `cassa`
---
 
 CREATE TABLE IF NOT EXISTS `cassa` (
   `idmovimento` int(10) unsigned NOT NULL AUTO_INCREMENT,
@@ -37,24 +19,12 @@ CREATE TABLE IF NOT EXISTS `cassa` (
   KEY `fk_cassa_ordini1_idx` (`idordine`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `categorie`
---
-
 CREATE TABLE IF NOT EXISTS `categorie` (
   `idcat` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `descrizione` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
   `aliquota_iva` tinyint(4) NOT NULL,
   PRIMARY KEY (`idcat`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `categorie_sub`
---
 
 CREATE TABLE IF NOT EXISTS `categorie_sub` (
   `idsubcat` int(10) unsigned NOT NULL AUTO_INCREMENT,
@@ -66,12 +36,6 @@ CREATE TABLE IF NOT EXISTS `categorie_sub` (
   KEY `fk_categorie_sub_produttori1_idx` (`idproduttore`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `groups`
---
-
 CREATE TABLE IF NOT EXISTS `groups` (
   `idgroup` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `nome` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
@@ -79,15 +43,8 @@ CREATE TABLE IF NOT EXISTS `groups` (
   `provincia` char(2) COLLATE utf8_unicode_ci NOT NULL,
   `data_creazione` date NOT NULL,
   `email_ml` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`idgroup`),
-  KEY `fk_groups_province1_idx` (`provincia`)
+  PRIMARY KEY (`idgroup`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `listini`
---
 
 CREATE TABLE IF NOT EXISTS `listini` (
   `idlistino` int(10) unsigned NOT NULL AUTO_INCREMENT,
@@ -99,12 +56,6 @@ CREATE TABLE IF NOT EXISTS `listini` (
   PRIMARY KEY (`idlistino`),
   KEY `fk_listini_produttori1_idx` (`idproduttore`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `listini_groups`
---
 
 CREATE TABLE IF NOT EXISTS `listini_groups` (
   `idlistino` int(10) unsigned NOT NULL,
@@ -119,12 +70,6 @@ CREATE TABLE IF NOT EXISTS `listini_groups` (
   KEY `fk_groups_listini_groups1_idx` (`idgroup_master`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `listini_prodotti`
---
-
 CREATE TABLE IF NOT EXISTS `listini_prodotti` (
   `idlistino` int(10) unsigned NOT NULL,
   `idprodotto` int(10) unsigned NOT NULL,
@@ -137,15 +82,9 @@ CREATE TABLE IF NOT EXISTS `listini_prodotti` (
   KEY `fk_prodotti_listini_listini1_idx` (`idlistino`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `ordini`
---
-
 CREATE TABLE IF NOT EXISTS `ordini` (
   `idordine` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `iduser_ref` int(10) unsigned NOT NULL,
+  `iduser_supervisore` int(10) unsigned NOT NULL,
   `descrizione` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `data_inizio` datetime NOT NULL,
   `data_fine` datetime NOT NULL,
@@ -155,35 +94,23 @@ CREATE TABLE IF NOT EXISTS `ordini` (
   `archiviato` enum('N','S') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'N',
   `condivisione` enum('PUB','PRI','SHA') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'PRI',
   PRIMARY KEY (`idordine`),
-  KEY `fk_ordini_users1_idx` (`iduser_ref`)
+  KEY `fk_ordini_users1` (`iduser_supervisore`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `ordini_groups`
---
 
 CREATE TABLE IF NOT EXISTS `ordini_groups` (
   `idordine` int(10) unsigned NOT NULL,
   `idgroup_master` int(10) unsigned NOT NULL,
   `idgroup_slave` int(10) unsigned NOT NULL,
-  `iduser_ref` int(10) unsigned DEFAULT NULL,
+  `iduser_incaricato` int(10) unsigned DEFAULT NULL,
   `visibile` enum('S','N') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'S',
   `extra` varchar(1024) COLLATE utf8_unicode_ci NOT NULL,
   `note_consegna` text COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`idordine`,`idgroup_master`,`idgroup_slave`),
   KEY `fk_groups_ordini_ordini1_idx` (`idordine`),
-  KEY `fk_groups_ordini_users1_idx` (`iduser_ref`),
   KEY `fk_groups_ordini_groups2_idx` (`idgroup_slave`),
-  KEY `fk_groups_ordini_groups1_idx` (`idgroup_master`)
+  KEY `fk_groups_ordini_groups1_idx` (`idgroup_master`),
+  KEY `fk_groups_ordini_users1_idx` (`iduser_incaricato`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `ordini_prodotti`
---
 
 CREATE TABLE IF NOT EXISTS `ordini_prodotti` (
   `idordine` int(10) unsigned NOT NULL,
@@ -198,12 +125,6 @@ CREATE TABLE IF NOT EXISTS `ordini_prodotti` (
   KEY `fk_ordini_prodotti_prodotti_listini1_idx` (`idlistino`,`idprodotto`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `ordini_users`
---
-
 CREATE TABLE IF NOT EXISTS `ordini_users` (
   `iduser` int(10) unsigned NOT NULL,
   `idordine` int(10) unsigned NOT NULL,
@@ -212,12 +133,6 @@ CREATE TABLE IF NOT EXISTS `ordini_users` (
   KEY `fk_ordini_user_ordini1_idx` (`idordine`),
   KEY `fk_ordini_user_ordini2_idx` (`iduser`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `ordini_user_prodotti`
---
 
 CREATE TABLE IF NOT EXISTS `ordini_user_prodotti` (
   `iduser` int(10) unsigned NOT NULL,
@@ -232,12 +147,6 @@ CREATE TABLE IF NOT EXISTS `ordini_user_prodotti` (
   KEY `fk_ordini_user_prodotti_ordini_user1_idx` (`iduser`,`idordine`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `ordini_variazioni`
---
-
 CREATE TABLE IF NOT EXISTS `ordini_variazioni` (
   `idov` int(11) NOT NULL AUTO_INCREMENT,
   `idordine` int(10) unsigned NOT NULL,
@@ -248,12 +157,6 @@ CREATE TABLE IF NOT EXISTS `ordini_variazioni` (
   KEY `fk_ordini_variazioni_ordini1_idx` (`idordine`),
   KEY `fk_ordini_variazioni_ordini_user1_idx` (`iduser`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `prodotti`
---
 
 CREATE TABLE IF NOT EXISTS `prodotti` (
   `idprodotto` int(10) unsigned NOT NULL AUTO_INCREMENT,
@@ -275,12 +178,6 @@ CREATE TABLE IF NOT EXISTS `prodotti` (
   KEY `fk_prodotti_users1_idx` (`iduser_creator`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `produttori`
---
-
 CREATE TABLE IF NOT EXISTS `produttori` (
   `idproduttore` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `ragsoc` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
@@ -291,15 +188,8 @@ CREATE TABLE IF NOT EXISTS `produttori` (
   `email` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `note` varchar(2048) COLLATE utf8_unicode_ci NOT NULL,
   `production` enum('S','N') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'N',
-  PRIMARY KEY (`idproduttore`),
-  KEY `fk_produttori_province1_idx` (`provincia`)
+  PRIMARY KEY (`idproduttore`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `province`
---
 
 CREATE TABLE IF NOT EXISTS `province` (
   `provincia` char(2) COLLATE utf8_unicode_ci NOT NULL,
@@ -307,12 +197,6 @@ CREATE TABLE IF NOT EXISTS `province` (
   `provdesc` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`provincia`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `referenti`
---
 
 CREATE TABLE IF NOT EXISTS `referenti` (
   `idgroup` int(10) unsigned NOT NULL,
@@ -322,12 +206,6 @@ CREATE TABLE IF NOT EXISTS `referenti` (
   KEY `fk_referenti_produttori1_idx` (`idproduttore`),
   KEY `fk_referenti_users1_idx` (`iduser_ref`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
 
 CREATE TABLE IF NOT EXISTS `users` (
   `iduser` int(10) unsigned NOT NULL AUTO_INCREMENT,
@@ -340,15 +218,8 @@ CREATE TABLE IF NOT EXISTS `users` (
   `provincia` char(2) COLLATE utf8_unicode_ci NOT NULL,
   `role` enum('User','Admin') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'User',
   PRIMARY KEY (`iduser`),
-  KEY `INDEX_email` (`email`),
-  KEY `fk_users_province1_idx` (`provincia`)
+  KEY `INDEX_email` (`email`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users_group`
---
 
 CREATE TABLE IF NOT EXISTS `users_group` (
   `iduser` int(10) unsigned NOT NULL,
@@ -361,12 +232,6 @@ CREATE TABLE IF NOT EXISTS `users_group` (
   KEY `FK_user_idx2` (`iduser`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `users_produttori`
---
-
 CREATE TABLE IF NOT EXISTS `users_produttori` (
   `iduser` int(10) unsigned NOT NULL,
   `idproduttore` int(10) unsigned NOT NULL,
@@ -376,114 +241,66 @@ CREATE TABLE IF NOT EXISTS `users_produttori` (
   KEY `FK_produttore_idx` (`idproduttore`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
---
--- Constraints for dumped tables
---
 
---
--- Constraints for table `cassa`
---
 ALTER TABLE `cassa`
   ADD CONSTRAINT `fk_cassa_ordini1` FOREIGN KEY (`idordine`) REFERENCES `ordini` (`idordine`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_cassa_users1` FOREIGN KEY (`iduser`) REFERENCES `users` (`iduser`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
---
--- Constraints for table `categorie_sub`
---
 ALTER TABLE `categorie_sub`
   ADD CONSTRAINT `fk_categorie_sub_categorie1` FOREIGN KEY (`idcat`) REFERENCES `categorie` (`idcat`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_categorie_sub_produttori1` FOREIGN KEY (`idproduttore`) REFERENCES `produttori` (`idproduttore`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
---
--- Constraints for table `listini`
---
 ALTER TABLE `listini`
   ADD CONSTRAINT `fk_listini_produttori1` FOREIGN KEY (`idproduttore`) REFERENCES `produttori` (`idproduttore`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
---
--- Constraints for table `listini_groups`
---
 ALTER TABLE `listini_groups`
   ADD CONSTRAINT `fk_groups_listini_listini1` FOREIGN KEY (`idlistino`) REFERENCES `listini` (`idlistino`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_groups_produttori_groups1` FOREIGN KEY (`idgroup_master`) REFERENCES `groups` (`idgroup`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_groups_produttori_groups2` FOREIGN KEY (`idgroup_slave`) REFERENCES `groups` (`idgroup`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
---
--- Constraints for table `listini_prodotti`
---
 ALTER TABLE `listini_prodotti`
   ADD CONSTRAINT `fk_prodotti_listini_listini1` FOREIGN KEY (`idlistino`) REFERENCES `listini` (`idlistino`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_prodotti_listini_prodotti1` FOREIGN KEY (`idprodotto`) REFERENCES `prodotti` (`idprodotto`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
---
--- Constraints for table `ordini`
---
 ALTER TABLE `ordini`
-  ADD CONSTRAINT `fk_ordini_users1` FOREIGN KEY (`iduser_ref`) REFERENCES `users` (`iduser`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `fk_ordini_users1` FOREIGN KEY (`iduser_supervisore`) REFERENCES `users` (`iduser`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
---
--- Constraints for table `ordini_groups`
---
 ALTER TABLE `ordini_groups`
   ADD CONSTRAINT `fk_groups_ordini_groups1` FOREIGN KEY (`idgroup_master`) REFERENCES `groups` (`idgroup`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_groups_ordini_groups2` FOREIGN KEY (`idgroup_slave`) REFERENCES `groups` (`idgroup`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_groups_ordini_ordini1` FOREIGN KEY (`idordine`) REFERENCES `ordini` (`idordine`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_groups_ordini_users1` FOREIGN KEY (`iduser_ref`) REFERENCES `users` (`iduser`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `fk_groups_ordini_users1` FOREIGN KEY (`iduser_incaricato`) REFERENCES `users` (`iduser`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
---
--- Constraints for table `ordini_prodotti`
---
 ALTER TABLE `ordini_prodotti`
   ADD CONSTRAINT `fk_ordini_prodotti_ordini1` FOREIGN KEY (`idordine`) REFERENCES `ordini` (`idordine`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_ordini_prodotti_prodotti_listini1` FOREIGN KEY (`idlistino`, `idprodotto`) REFERENCES `listini_prodotti` (`idlistino`, `idprodotto`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
---
--- Constraints for table `ordini_users`
---
 ALTER TABLE `ordini_users`
   ADD CONSTRAINT `fk_ordini_user_ordini1` FOREIGN KEY (`idordine`) REFERENCES `ordini` (`idordine`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_ordini_user_users1` FOREIGN KEY (`iduser`) REFERENCES `users` (`iduser`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
---
--- Constraints for table `ordini_user_prodotti`
---
 ALTER TABLE `ordini_user_prodotti`
-  ADD CONSTRAINT `fk_ordini_user_prodotti_ordini_user1` FOREIGN KEY (`iduser`, `idordine`) REFERENCES `ordini_users` (`iduser`, `idordine`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_ordini_user_prodotti_ordini_prodotti1` FOREIGN KEY (`idlistino`, `idprodotto`) REFERENCES `ordini_prodotti` (`idlistino`, `idprodotto`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `fk_ordini_user_prodotti_ordini_prodotti1` FOREIGN KEY (`idlistino`, `idprodotto`) REFERENCES `ordini_prodotti` (`idlistino`, `idprodotto`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_ordini_user_prodotti_ordini_user1` FOREIGN KEY (`iduser`, `idordine`) REFERENCES `ordini_users` (`iduser`, `idordine`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
---
--- Constraints for table `ordini_variazioni`
---
 ALTER TABLE `ordini_variazioni`
   ADD CONSTRAINT `fk_ordini_variazioni_ordini1` FOREIGN KEY (`idordine`) REFERENCES `ordini` (`idordine`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_ordini_variazioni_ordini_user1` FOREIGN KEY (`iduser`) REFERENCES `ordini_users` (`iduser`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
---
--- Constraints for table `prodotti`
---
 ALTER TABLE `prodotti`
   ADD CONSTRAINT `FK_produttore_prodotti` FOREIGN KEY (`idproduttore`) REFERENCES `produttori` (`idproduttore`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_prodotti_sub_categorie1` FOREIGN KEY (`idsubcat`) REFERENCES `categorie_sub` (`idsubcat`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_prodotti_users1` FOREIGN KEY (`iduser_creator`) REFERENCES `users` (`iduser`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
---
--- Constraints for table `referenti`
---
 ALTER TABLE `referenti`
   ADD CONSTRAINT `fk_referenti_groups1` FOREIGN KEY (`idgroup`) REFERENCES `groups` (`idgroup`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_referenti_produttori1` FOREIGN KEY (`idproduttore`) REFERENCES `produttori` (`idproduttore`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_referenti_users1` FOREIGN KEY (`iduser_ref`) REFERENCES `users` (`iduser`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
---
--- Constraints for table `users_group`
---
 ALTER TABLE `users_group`
   ADD CONSTRAINT `FK_group_users_group` FOREIGN KEY (`idgroup`) REFERENCES `groups` (`idgroup`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `FK_user_users_group` FOREIGN KEY (`iduser`) REFERENCES `users` (`iduser`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
---
--- Constraints for table `users_produttori`
---
 ALTER TABLE `users_produttori`
   ADD CONSTRAINT `FK_produttore` FOREIGN KEY (`idproduttore`) REFERENCES `produttori` (`idproduttore`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `FK_user_produttori` FOREIGN KEY (`iduser`) REFERENCES `users` (`iduser`) ON DELETE NO ACTION ON UPDATE NO ACTION;
