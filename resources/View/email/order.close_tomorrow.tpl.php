db5ae5c47e02438fe2f7849940a783f1f0bc281e
Davide Gullo <jazzo72@gmail.com>
2016-02-24 16:48:39 +0100

    added categorie e prodotti on notifica "Chiusura"

--- resources/View/email/order.close_tomorrow.tpl.php
+++ resources/View/email/order.close_tomorrow.tpl.php
@@ -2,6 +2,18 @@
 Domani <b><?php echo $this->date($this->ordine->getDataFine(), '%d/%m/%Y');?> alle <?php echo $this->date($this->ordine->getDataFine(), '%H:%M');?></b><br />
 verrà <b>Chiuso</b> l'ordine <b><?php echo $this->ordine->getDescrizione(); ?></b>.<br />
 <br />
+<?php 
+    $categorie = $this->ordine->getListaDescrizioniCategorie();
+    if( count($categorie) ): ?>
+Prodotti: <b><?php echo implode(", ", $categorie); ?></b><br />
+<?php endif; ?>
+<?php 
+    $produttori = $this->ordine->getProduttoriList();
+    if( count($produttori) ): ?>
+Produttori: <b><?php echo implode(", ", $produttori); ?></b><br />
+<?php endif; ?>
+<br />
+<br />
 Note consegna:<br />
 <?php echo $this->note_consegna; ?>
 <br />
